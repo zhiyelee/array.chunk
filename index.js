d1091d67456c5ab7f8b134f7d1388f383a53b26a
zhiyelee <zhiyelee@gmail.com>
2016-03-31 11:21:09 -0500

    add TypedArray support
    use subarray if slice is not available for TypedArray

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
+/**
+ * get type string
+ * @param val
+ * @returns {string}
+ */
+function getType(val) {
+  // `[object Array]`.slice(8, -1) ==> Array
+  return Object.prototype.toString.call(val).slice(8, -1);
+}
+
+/**
+ * check whether arr is an Array of TypedArray
+ * @param arr
+ * @return bool
+ */
+function isArray(arr) {
+  var type = getType(arr);
+  var arrTypes = [
+    'Array',
+    'Int8Array',
+    'Uint8Array',
+    'Uint8ClampedArray',
+    'Int16Array',
+    'Uint16Array',
+    'Int32Array',
+    'Uint32Array',
+    'Float32Array',
+    'Float64Array'
+  ];
+
+  return arrTypes.indexOf(type) !== -1;
+
+}
 module.exports = function chunks(arr, size) {
-  if (!Array.isArray(arr)) {
-    throw new TypeError('Input should be Array');
+  if (!isArray(arr)) {
+    throw new TypeError('Input should be Array or TypedArray');
   }
 
   if (typeof size !== 'number') {
@@ -9,7 +42,11 @@ module.exports = function chunks(arr, size) {
 
   var result = [];
   for (var i = 0; i < arr.length; i += size) {
-    result.push(arr.slice(i, size + i));
+    if (typeof arr.slice === 'function') {
+      result.push(arr.slice(i, size + i));
+    } else {
+      result.push(arr.subarray(i, size + i));
+    }
   }
 
   return result;
